925c871530abaa08fe216d72f97bba90103692f5
xianwei li <xil148@pitt.edu>
2017-09-21 11:51:04 -0400

    add recyclerview and test recyclerview and queryutils

---
+++ app/src/main/java/com/xianwei/bakingapp/adapters/RecipeAdapter.java
@@ -0,0 +1,10 @@
+package com.xianwei.bakingapp;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by xianwei li on 9/20/2017.
+ */
+
+public class recipeAdater extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/xianwei/bakingapp/loaders/RecipeLoader.java
@@ -0,0 +1,10 @@
+package com.xianwei.bakingapp.loaders;
+
+import android.content.AsyncTaskLoader;
+
+/**
+ * Created by xianwei li on 9/20/2017.
+ */
+
+public class recipeLoader extends AsyncTaskLoader {
+}
